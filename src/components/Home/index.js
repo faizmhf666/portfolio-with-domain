57fd47ba5c73b0e6b318213b22a0bec4a6f5c7b2
jordanfaiz <faizmhf666@gmail.com>
2023-08-17 12:39:31 +0530

    added button styles and fixed previous issues

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,10 +3,38 @@ import { Link } from "react-router-dom";
 // import LocomotiveScroll from 'locomotive-scroll';
 import Navbar from "../Navbar";
 import Footer from "../Footer";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import Popup from 'reactjs-popup'
+
+import 'reactjs-popup/dist/index.css'
 import "./HomeContent.css";
+
 // import 'locomotive-scroll.css'
 
 const Home = () => {
+  const form = useRef();
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_0sscxso",
+        "template_eddo0jj",
+        form.current,
+        "2OtesAymt5wLGrsSz"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  }
 
   return (
     <>
@@ -32,11 +60,75 @@ const Home = () => {
                 Get Resume
               </button>
             </Link>
-            <Link to="/Contact" data-scroll>
-              <button type="button" className="btn btn-2 arrow">
-                Contact
-              </button>
-            </Link>
+            <div className="popup-container">
+   <Popup
+     modal
+     trigger={
+       <button type="button" className="trigger-button btn btn-2 arrow">
+         Contact Me
+       </button>
+     }
+   >
+     {close => (
+       <>
+         <div className="home-form">
+        <form onSubmit={onSubmitForm} ref={form}>
+          <div className="form-element">
+            <label htmlFor="name" className="form-label">
+              Your Name*
+            </label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Your Name Here"
+              className="input-box"
+            />
+          </div>
+          <div className="form-element">
+            <label htmlFor="email" className="form-label">
+              Email*
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              placeholder="Your Email Here"
+              className="input-box"
+            />
+          </div>
+          <div className="form-element">
+            <label htmlFor="Message" className="form-label">
+              Message*
+            </label>
+            <textarea
+              type="text"
+              id="Message"
+              name="Message"
+              rows="7"
+              cols="50"
+              placeholder="Your Message Here"
+              className="input-box"
+            />
+          </div>
+          <div className="home-form-button">
+          <button type="submit" className="btn btn-1 arrow">
+            Send
+          </button>
+          <button
+           type="button"
+           className="trigger-button btn btn-2 arrow"
+           onClick={() => close()}
+         >
+           Close
+         </button>
+          </div>
+        </form>
+         </div>
+       </>
+     )}
+   </Popup>
+ </div>
           </div>
         </div>
       </div>
